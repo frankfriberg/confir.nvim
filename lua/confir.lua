db82f0d6eedc692f650ca524fccaac25edcb30f9
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2024-04-24 07:21:37 +0200

    feat: run stylua formatter

--- lua/confir.lua
+++ lua/confir.lua
@@ -3,14 +3,14 @@ local confirm = require("confir.confirm")
 local M = {}
 
 local update = function(user_config)
-  vim.tbl_deep_extend("force", config, user_config or {})
-  return config
+	vim.tbl_deep_extend("force", config, user_config or {})
+	return config
 end
 
 M.setup = function(user_config)
-  config = update(user_config)
+	config = update(user_config)
 
-  vim.fn.confirm = confirm.confir
+	vim.fn.confirm = confirm.confir
 end
 
 return M
--- lua/confir/util.lua
+++ lua/confir/util.lua
@@ -1,5 +1,5 @@
 local config = require("confir.config")
-M = {}
+local M = {}
 
 local win_options = config.win_options
 local relative = win_options.relative
@@ -9,114 +9,114 @@ local is_cursor = relative == "cursor"
 local is_win = relative == "win"
 
 local relative_height = function()
-  if is_cursor then
-    return vim.fn.winline()
-  elseif is_win then
-    return vim.api.nvim_win_get_height(0)
-  else
-    return vim.o.lines
-  end
+	if is_cursor then
+		return vim.fn.winline()
+	elseif is_win then
+		return vim.api.nvim_win_get_height(0)
+	else
+		return vim.o.lines
+	end
 end
 
 local relative_width = function()
-  if is_win then
-    return vim.api.nvim_win_get_width(0)
-  else
-    return vim.o.columns
-  end
+	if is_win then
+		return vim.api.nvim_win_get_width(0)
+	else
+		return vim.o.columns
+	end
 end
 
 local get_left_position = function(win_width)
-  if is_cursor then
-    return -win_width + margin.left + 1
-  else
-    return margin.left or 0
-  end
+	if is_cursor then
+		return -win_width + margin.left + 1
+	else
+		return margin.left or 0
+	end
 end
 
 local get_center_position = function(win_width)
-  if is_cursor then
-    return -(win_width / 2)
-  else
-    return (relative_width() - win_width) / 2
-  end
+	if is_cursor then
+		return -(win_width / 2)
+	else
+		return (relative_width() - win_width) / 2
+	end
 end
 
 local get_right_position = function(win_width)
-  if is_cursor then
-    return win_width + margin.right
-  else
-    return relative_width() - win_width - margin.right
-  end
+	if is_cursor then
+		return win_width + margin.right
+	else
+		return relative_width() - win_width - margin.right
+	end
 end
 
 local get_top_position = function(win_height)
-  if is_cursor then
-    return -win_height - margin.top - 1
-  else
-    return margin.top
-  end
+	if is_cursor then
+		return -win_height - margin.top - 1
+	else
+		return margin.top
+	end
 end
 
 local get_middle_position = function(win_height)
-  if is_cursor then
-    return win_height / 2
-  else
-    return (relative_height() - win_height) / 2
-  end
+	if is_cursor then
+		return win_height / 2
+	else
+		return (relative_height() - win_height) / 2
+	end
 end
 
 local get_bottom_position = function()
-  if is_cursor then
-    return margin.bottom + 1
-  else
-    return relative_height() - vim.o.cmdheight - margin.bottom
-  end
+	if is_cursor then
+		return margin.bottom + 1
+	else
+		return relative_height() - vim.o.cmdheight - margin.bottom
+	end
 end
 
 M.add_padding_left = function(text, win_width)
-  local text_width = text:len()
-  local left_padding = (win_width - text_width) / 2
-  local left_spaces = string.rep(" ", left_padding)
-  return left_spaces .. text
+	local text_width = text:len()
+	local left_padding = (win_width - text_width) / 2
+	local left_spaces = string.rep(" ", left_padding)
+	return left_spaces .. text
 end
 
 M.positions = {
-  ["bottom-left"] = {
-    row = get_bottom_position,
-    col = get_left_position,
-    anchor = "SW",
-  },
-  ["bottom-center"] = {
-    row = get_bottom_position,
-    col = get_center_position,
-    anchor = "SW",
-  },
-  ["bottom-right"] = {
-    row = get_bottom_position,
-    col = get_right_position,
-    anchor = "SE",
-  },
-  ["top-left"] = {
-    row = get_top_position,
-    col = get_left_position,
-    anchor = "NW",
-  },
-  ["top-center"] = {
-    row = get_top_position,
-    col = get_center_position,
-    anchor = "NW",
-  },
-  ["top-right"] = {
-    row = get_top_position,
-    col = get_right_position,
-    anchor = "NE",
-  },
-  ["center-center"] = {
-    row = get_middle_position,
-    col = get_center_position,
-    anchor = "NW",
-  }
+	["bottom-left"] = {
+		row = get_bottom_position,
+		col = get_left_position,
+		anchor = "SW",
+	},
+	["bottom-center"] = {
+		row = get_bottom_position,
+		col = get_center_position,
+		anchor = "SW",
+	},
+	["bottom-right"] = {
+		row = get_bottom_position,
+		col = get_right_position,
+		anchor = "SE",
+	},
+	["top-left"] = {
+		row = get_top_position,
+		col = get_left_position,
+		anchor = "NW",
+	},
+	["top-center"] = {
+		row = get_top_position,
+		col = get_center_position,
+		anchor = "NW",
+	},
+	["top-right"] = {
+		row = get_top_position,
+		col = get_right_position,
+		anchor = "NE",
+	},
+	["center-center"] = {
+		row = get_middle_position,
+		col = get_center_position,
+		anchor = "NW",
+	},
 }
 
 return M
