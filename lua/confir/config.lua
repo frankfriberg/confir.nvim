2f5ad5cabedb481d6b7427b106e95bdd7deca82d
Frank Friberg <3509803+frankfriberg@users.noreply.github.com>
2024-05-27 14:15:45 +0200

    wip

--- lua/confir/config.lua
+++ lua/confir/config.lua
@@ -1,13 +1,7 @@
-local ns_id = vim.api.nvim_create_namespace("confir")
+local C = {}
 
-if vim.api.nvim_win_set_hl_ns then
-	vim.api.nvim_set_hl(ns_id, "NormalFloat", { bg = "NONE" })
-	vim.api.nvim_set_hl(ns_id, "FloatBorder", { bg = "NONE" })
-end
-
-local config = {
+local options = {
 	max_width = 40,
-	ns_id = ns_id,
 	types = {
 		error = {
 			icon = "󰅚",
@@ -23,11 +17,11 @@ local config = {
 		},
 		warning = {
 			icon = "",
-			color = "Comment",
+			color = "WarningFloat",
 		},
 		generic = {
 			icon = "",
-			color = "WarningFloat",
+			color = "Comment",
 		},
 	},
 	win_options = {
@@ -45,4 +39,13 @@ local config = {
 	},
 }
 
-return config
+C.set = function(user_config)
+	options = vim.tbl_deep_extend("force", options, user_config or {})
+	return options
+end
+
+C.get = function()
+	return options
+end
+
+return C
